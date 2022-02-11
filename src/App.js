959d16f4a536568ab7726e08b2bb81b47f88dd33
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2022-02-11 20:05:18 +0700

    + keterangan load &  jika tidak / belum ada data

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
-  function fetchDataMovieHandler() {
-    fetch("https://swapi.dev/api/films").then( (response) => {
-      return response.json();
-    }).then((data) => {
-      const movieList = data.results.map((data) => {
-        return {
-          id: data.episode_id,
-          title : data.title,
-          releaseDate : data.created,
-          openingText : data.opening_crawl
-        };
-      });
-      setMovies(movieList);
+  async function fetchDataMovieHandler() {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films");
+    const data  = await response.json();
+  
+    const movieList = data.results.map((data) => {
+      return {
+        id: data.episode_id,
+        title : data.title,
+        releaseDate : data.created,
+        openingText : data.opening_crawl
+      };
     });
+    
+    setMovies(movieList);
+    setIsLoading(false);
   }
 
   // console.log(movies);
@@ -30,7 +33,9 @@ function App() {
         <button onClick={fetchDataMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found No Movies</p>}
+        {isLoading && <p> Loading .... </p>}
       </section>
     </React.Fragment>
   );
