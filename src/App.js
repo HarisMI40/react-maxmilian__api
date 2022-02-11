bdacc5d05b9b084f92c83a09f4452b49e4a5d026
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2022-02-11 22:49:39 +0700

    update jadi menggunakan firebase

--- src/App.js
+++ src/App.js
@@ -1,51 +1,72 @@
-import React,{useState} from 'react';
-
+import React,{useState, useEffect, useCallback} from 'react';
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 import './App.css';
-import PulseLoader from "react-spinners/PulseLoader";
+import SyncLoader from "react-spinners/SyncLoader";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchDataMovieHandler() {
+  const fetchDataMovieHandler = useCallback(async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data  = await response.json();
-  
-    const movieList = data.results.map((data) => {
-      return {
-        id: data.episode_id,
-        title : data.title,
-        releaseDate : data.created,
-        openingText : data.opening_crawl
-      };
-    });
-    
-    setMovies(movieList);
-    setIsLoading(false);
-  }
-  // let content;
-  // if(!isLoading && movies.length > 0){
-  //   content = <MoviesList movies={movies} />
-  // }else if(!isLoading && movies.length === 0){
-  //   content = <p>Found No Movies</p>;
-  // }else if(isLoading){
-  //   content = <p> Loading .... </p>;
-  // }
-  // setelah dicoba, ternyata lebih bagus menggunakan yang di bawah, lebih cepat dan gak error
-  // let [loading, setLoading] = useState(true);
+    setError(null);
+    try{
+      const response = await fetch("https://react-bd3d8-default-rtdb.firebaseio.com/movies.json");
+      if(!response.ok){
+        throw new Error("something went wrong");
+      }
 
+   
+      const data  = await response.json();
+      const loadedDataMovies = [];
+      for(const key in data){
+        loadedDataMovies.push({
+          id : key,
+          title : data[key].title,
+          openingText : data[key].openingText,
+          releaseDate : data[key].releaseDate
+        });
+      }
+      
+      setMovies(loadedDataMovies);
+    }catch(error){
+      setError(error.message);
+    }
+    setIsLoading(false);
+  },[]);
+
+  useEffect(() => {
+    fetchDataMovieHandler();
+  }, [fetchDataMovieHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch("https://react-bd3d8-default-rtdb.firebaseio.com/movies.json", {
+      method:'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-type' : 'application/json'
+      }
+    });
+
+    const data  = await response.json();
+    console.log(data);
+  }
+  
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchDataMovieHandler}>Fetch Movies</button>
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found No Movies</p>}
-        {isLoading && <PulseLoader color={"#230052"} loading={isLoading} size={20} />}
-      
+        {!isLoading && movies.length === 0 && !error && <p>Found No Movies</p>}
+        {!isLoading && error && <p> {error} </p>}
+        {isLoading && <SyncLoader color={"#230052"} loading={isLoading} size={20} />}
       </section>
     </React.Fragment>
   );
